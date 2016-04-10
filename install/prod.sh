cfd9ab33b18bce8e508c87e6a4271533dd73fd60
Brad Fisher <bradallenfisher@gmail.com>
2016-04-10 15:19:59 -0400

    Update prod.sh

--- install/prod.sh
+++ install/prod.sh
@@ -102,3 +102,34 @@ systemctl start varnish.service
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 ln -s /usr/local/bin/composer /usr/bin/composer
+
+# Drush Local
+composer global require drush/drush:8.*
+echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> $HOME/.bashrc
+source $HOME/.bashrc
+
+# Install Drupal Console
+curl https://drupalconsole.com/installer -L -o drupal.phar
+mv drupal.phar /usr/local/bin/drupal
+chmod +x /usr/local/bin/drupal
+ln -s /usr/local/bin/drupal /usr/bin/drupal
+
+drupal init
+# install drush recipes
+#drush dl drush_recipes -y
+#drush dl drush_cleanup
+drush cc drush
+
+drush dl drupal-8 --destination=/var/www/ --drupal-project-rename=html -y
+
+cd /var/www/html
+drush site-install --account-pass=admin --db-url=mysql://root@localhost:22/test -y
+sudo chmod -R 755 /var/www/html/sites/default/files/
+sudo chown -R apache:apache /var/www/html/sites/default/files/
+
+#start the apache service now that we have an .htaccess file
+sudo systemctl start httpd.service
+
+sudo service firewalld stop
+echo "DUNZY!"
+exec bash
